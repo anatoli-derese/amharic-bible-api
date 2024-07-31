8983018a2a54f40554be12ee1940b9a01fe56638
anatoli-derese <anatoli.derese@aait.edu.et>
2024-07-31 21:53:51 +0300

    swagger UI documentation added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,26 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+// async function bootstrap() {
+//   const app = await NestFactory.create(AppModule);
+//   await app.listen(3000);
+// }
+// bootstrap();
+
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Amharic Bible Api')
+    .setDescription('Amharic Bible Api Description')
+    .setVersion('1.0')
+    .addTag('books')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
